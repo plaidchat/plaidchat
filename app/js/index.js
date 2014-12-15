c9dbc70c9115bab90db7dc25a82e9990c67c5ead
Julian Haldenby <j.haldenby@gmail.com>
2014-12-15 16:18:23 -0500

    Remove log lines

--- app/js/index.js
+++ app/js/index.js
@@ -4,13 +4,12 @@
 	var validSlackRedirect = /(.+\.)?slack-redir.com/i;
 
 	win.on('new-win-policy',function (frame, url, policy) {
-		console.log(policy);
 		var openRequest = urllib.parse(url);
 
 		if ( validSlackRedirect.test(openRequest.host)) {
 			gui.Shell.openExternal(url);
 			policy.ignore();
-			console.log('Requested to open: '+ JSON.stringify(openRequest));
+			console.log('Allowing browser to handle: '+ JSON.stringify(openRequest));
 		}
 		
 	});
