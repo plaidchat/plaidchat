155d22f6ce6d327d661554a147cafb978684586f
Julian Haldenby <j.haldenby@gmail.com>
2014-12-23 17:56:41 -0500

    Begining of fix for rembering the last known team

--- app/js/index.js
+++ app/js/index.js
@@ -1,6 +1,11 @@
-(function(gui,urllib) {
+/*global window*/
+var webslack = (function(gui,urllib,request,localStorage) {
 	'use strict';
+
+	var webslack = {};
+
 	var win = gui.Window.get();
+	var validSlackSubdomain = /https?:\/\/(.+\.)?slack.com(\/.*)?/i;
 	var validSlackRedirect = /(.+\.)?slack-redir.com/i;
 
 	win.on('new-win-policy',function (frame, url, policy) {
@@ -11,6 +16,28 @@
 			policy.ignore();
 			console.log('Allowing browser to handle: '+ JSON.stringify(openRequest));
 		}
-		
 	});
-})(require('nw.gui'),require('url'));
+
+    var newLocationToProcess = function(msg)
+    {
+     	if ( validSlackSubdomain.test(msg) ) {
+     		var subdomain = validSlackSubdomain.exec(msg);
+     		if ( subdomain[0] && subdomain[0].length > 1 ) {
+     			var subdomainFiltered = subdomain[0].substr(0,subdomain[0].length);
+     			if ( subdomainFiltered === localStorage.currentDomain ) {
+     				localStorage.currentDomain = subdomainFiltered;
+     			}
+     		}
+     	}
+    };
+
+	win.on('document-end',function(frame) {
+		if ( frame && frame.contentWindow ) {
+			newLocationToProcess(frame.contentWindow.location.href);
+		}
+	});
+
+	webslack.load = function() {
+		
+	}
+})(require('nw.gui'),require('url'),localStorage);
