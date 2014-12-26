358472c0311240798ea4e215b5870b58a18701d6
Todd Wolfson <todd@twolfson.com>
2014-12-26 09:32:50 -0600

    Moved to grunt linters and added `.editorconfig` linting

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,33 @@
-'use strict';
-
 module.exports = function (grunt) {
-  grunt.loadNpmTasks('grunt-node-webkit-builder');
+	'use strict';
 
-  grunt.initConfig({
-    nodewebkit: {
-      options: {
-          platforms: ['linux64'],
-          buildDir: './webkitbuilds', // Where the build version of my node-webkit app is saved
-      },
-      src: ['./app/**/*'] // Your node-webkit app
-    },
-  });
+	// Load in grunt tasks with optional development tasks
+	grunt.loadNpmTasks('grunt-node-webkit-builder');
+	try {
+		grunt.loadNpmTasks('grunt-contrib-jshint');
+		grunt.loadNpmTasks('grunt-lintspaces');
+	} catch (err) {
+	}
+
+	grunt.initConfig({
+		nodewebkit: {
+			options: {
+				platforms: ['linux64'],
+				buildDir: './webkitbuilds' // Where the build version of my node-webkit app is saved
+			},
+			src: ['./app/**/*'] // Your node-webkit app
+		},
+		jshint: {
+			jshintrc: '.jshintrc',
+			all: ['*.js', 'app/**/*.js']
+		},
+		lintspaces: {
+			src: ['*', 'app/**/*', '!**/*.png'],
+			options: {
+				editorconfig: '.editorconfig'
+			}
+		}
+	});
+
+	grunt.registerTask('lint', ['jshint', 'lintspaces']);
 };
