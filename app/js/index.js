f1b44e43205aafee3816cf20ca389c8e4a185d16
Julian Haldenby <j.haldenby@gmail.com>
2014-12-27 03:32:05 -0500

    Added changes for saving the last visited slack subdomain into localStorage

--- app/js/index.js
+++ app/js/index.js
@@ -1,12 +1,16 @@
-/*global window*/
-var webslack = (function(gui,urllib,request,localStorage) {
+/*global document*/
+/*global localStorage*/
+/* exported webslack */
+var webslack = (function(gui,urllib,localStorage) {
 	'use strict';
-
+	var LOCAL_STORAGE_KEY_CURRENT_DOMAIN = 'currentDomain';
 	var webslack = {};
 
 	var win = gui.Window.get();
-	var validSlackSubdomain = /https?:\/\/(.+\.)?slack.com(\/.*)?/i;
+	var validSlackSubdomain = /https?:\/\/(.+)\.slack.com(\/.*)?/i;
+	var logoutSlackDomain   = /https?:\/\/slack.com(\/.*)?/i;
 	var validSlackRedirect = /(.+\.)?slack-redir.com/i;
+	var slackLoginUrl      = 'https://slack.com/signin';
 
 	win.on('new-win-policy',function (frame, url, policy) {
 		var openRequest = urllib.parse(url);
@@ -18,18 +22,38 @@ var webslack = (function(gui,urllib,request,localStorage) {
 		}
 	});
 
-    var newLocationToProcess = function(msg)
+    function newLocationToProcess(msg)
     {
      	if ( validSlackSubdomain.test(msg) ) {
      		var subdomain = validSlackSubdomain.exec(msg);
-     		if ( subdomain[0] && subdomain[0].length > 1 ) {
-     			var subdomainFiltered = subdomain[0].substr(0,subdomain[0].length);
-     			if ( subdomainFiltered === localStorage.currentDomain ) {
-     				localStorage.currentDomain = subdomainFiltered;
+     		if ( subdomain[1] && subdomain[1].length > 1 ) {
+     			var subdomainFiltered = subdomain[1];
+
+     			if ( subdomainFiltered !== localStorage.getItem( LOCAL_STORAGE_KEY_CURRENT_DOMAIN ) ) {
+     				localStorage.setItem( LOCAL_STORAGE_KEY_CURRENT_DOMAIN, subdomainFiltered );
      			}
      		}
+     	} else if ( logoutSlackDomain.test(msg) ) {
+     		localStorage.removeItem(LOCAL_STORAGE_KEY_CURRENT_DOMAIN);
      	}
-    };
+    }
+
+    function handleLoadIframe( url ) {
+    	var bodyElement = document.body;
+
+    	while (bodyElement.firstChild) {
+		    bodyElement.removeChild(bodyElement.firstChild);
+		}
+
+		var iframeDomElement = document.createElement('iframe');
+
+		iframeDomElement.setAttribute('src', url );
+		iframeDomElement.setAttribute('frameBorder','0');
+		iframeDomElement.setAttribute('nwdisable','');
+		iframeDomElement.setAttribute('nwfaketop','');
+
+		bodyElement.appendChild(iframeDomElement);
+    }
 
 	win.on('document-end',function(frame) {
 		if ( frame && frame.contentWindow ) {
@@ -37,7 +61,15 @@ var webslack = (function(gui,urllib,request,localStorage) {
 		}
 	});
 
+
 	webslack.load = function() {
-		
-	}
+		if ( localStorage.getItem( LOCAL_STORAGE_KEY_CURRENT_DOMAIN ) ) {
+			handleLoadIframe( 'https://' + localStorage.getItem( LOCAL_STORAGE_KEY_CURRENT_DOMAIN ) + '.slack.com/');	
+		} else {
+			handleLoadIframe(slackLoginUrl);
+		}
+	};
+
+
+	return webslack;
 })(require('nw.gui'),require('url'),localStorage);
