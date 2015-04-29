8649454f36967ab799e273cc09b1fc3c755ac578
wlaurance <w.laurance@gmail.com>
2015-04-28 19:15:20 -0700

    Set process title
    See #6

--- run.js
+++ run.js
@@ -1,4 +1,8 @@
 #!/usr/bin/env node
 var spawn = require('child_process').spawn;
 
-spawn(__dirname + '/./webkitbuilds/slack4linux/linux64/slack4linux');
+//Set the process title
+//So `killall node` doesn't kill slack-for-linux
+process.title = 'slack-for-linux';
+
+spawn(__dirname + '/./webkitbuilds/slack-for-linux/linux64/slack-for-linux');
