e743a013a2dfd50ecc6590a750dbd1758ab31e08
Luis Sagastume <lsagastume1990@gmail.com>
2015-06-09 16:44:52 -0600

    bug/fix audio notification

---
+++ postinstall.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#Exit on first error and output comments
+set -e
+set -x
+
+if test -f /opt/google/chrome/libffmpegsumo.so; then
+	cp /opt/google/chrome/libffmpegsumo.so node_modules/nw/nwjs/libffmpegsumo.so
+else
+	echo "Couldn't locate libffmpegsumo.so during installation. Audio notifications will be disabled" 1>&2
+	echo "To repair this, please install Google Chrome and reinstall \`slack-for-linux\`" 1>&2
+fi
