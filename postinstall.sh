92f6b1742306564a7d3d0e711af3c541793be67c
Todd Wolfson <todd@twolfson.com>
2016-01-19 23:03:40 -0600

    Moved to require.resolve for finding libffmpegsumo.so src and target

--- postinstall.sh
+++ postinstall.sh
@@ -5,7 +5,11 @@ set -x
 
 # Copy `libffmpegsumo.so` from electron to nw.js
 # DEV: We previously copied from Google Chrome, however they have stopped bundling `libffmpegsumo.so`
-cp node_modules/electron-prebuilt/dist/libffmpegsumo.so node_modules/nw/nwjs/libffmpegsumo.so
+# Example: `node_modules/electron-prebuilt/dist/libffmpegsumo.so`
+libffmpegsumo_src_path="$(node --eval "console.log(require.resolve('electron-prebuilt/dist/libffmpegsumo.so'))")"
+# Example: `node_modules/nwjs/libffmpegsumo.so`
+libffmpegsumo_target_path="$(node --eval "console.log(require('path').normalize(require.resolve('nw/nwjs/nw') + '/../libffmpegsumo.so'))")"
+cp "$libffmpegsumo_src_path" "$libffmpegsumo_target_path"
 
 # Compile our React for nw.js support
 npm run build
